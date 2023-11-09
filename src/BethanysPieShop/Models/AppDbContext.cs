777b7952c67c10430e07d533971ad2654f4d5ead
Abhay Chiradi <abhaychiradi727@gmail.com>
2023-11-09 16:01:25 +0530

    Update AppDbContext.cs

--- src/BethanysPieShop/Models/AppDbContext.cs
+++ src/BethanysPieShop/Models/AppDbContext.cs
@@ -21,4 +21,23 @@ namespace BethanysPieShop.Models
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
     }
+
+     public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {      
+
+        AppDbContext IDesignTimeDbContextFactory<AppDbContext>.CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<AppDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            return new AppDbContext(builder.Options);
+        }
+    }
 }
